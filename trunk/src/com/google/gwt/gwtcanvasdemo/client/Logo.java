c3ac6e28c01d70857dd0217e9c551e60bfa27916
pdr@google.com <pdr@google.com@0d06cd12-71b5-f05b-6dae-d4476c38183d>
2011-02-08 22:16:04 +0000

    Adding initial version of canvas demo

---
+++ trunk/src/com/google/gwt/gwtcanvasdemo/client/Logo.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2011 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.gwt.gwtcanvasdemo.client;
+
+import com.google.gwt.canvas.dom.client.Context2d;
+import com.google.gwt.dom.client.ImageElement;
+
+public class Logo extends SpringObject {
+  ImageElement image;
+  double width, height;
+  double rot;
+
+  Logo(ImageElement image, double imageWidth, double imageHeight) {
+    super(new Vector(0,0));
+    this.image = image;
+    this.width = imageWidth;
+    this.height = imageHeight;
+    this.rot = 0;
+  }
+  
+  void draw(Context2d context) {
+    context.save();
+    context.translate(this.pos.x, this.pos.y);
+    context.rotate(rot);
+    context.drawImage(image, 0, 0, width, height);
+    context.restore();
+  }
+}
